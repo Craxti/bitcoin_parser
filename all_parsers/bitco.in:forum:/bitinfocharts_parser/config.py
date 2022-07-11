149058eafe0239dc1635553b7b5aa9ebafdda67b
Aleksandr Fetisov <55095407+Craxti@users.noreply.github.com>
2022-07-11 13:19:44 +0400

    Delete all_parsers directory

--- all_parsers/bitco.in:forum:/bitinfocharts_parser/config.py
+++
@@ -1,5 +0,0 @@
-MONGO_URL = ''
-
-DOMAIN = 'https://bitinfocharts.com'
-URL_PART = '/top-100-richest-bitcoin-addresses-{id}.html'
-PAGES_COUNT = 100
--- all_parsers/bitco.in:forum:/bitinfocharts_parser/utils.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def get_first_from_list(l): 
-    return l[0] if l else None
-
-def remove_wallet_text(text):
-    return re.sub('wallet:', '', text, flags = re.IGNORECASE).strip()
